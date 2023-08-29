03f9d3fbbfaeec4a07f312f1a6a8dea14f195466
BARBOSA FARIAS Izael <barbosa.f.izael@gmail.com>
2023-08-29 15:42:05 +0200

    add hold function and newGame function in js file

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-let diceRoll = document.getElementById("diceRoll");
+const diceRoll = document.getElementById("diceRoll");
+const hold = document.getElementById("hold");
+const newGame = document.getElementById("newGame");
 let diceResult = document.getElementById("diceResult");
 let diceImage = document.getElementById("diceImage");
 let globalPlayer1 = document.getElementById("globalPlayer1");
@@ -21,27 +23,27 @@ const throwDice = () => {
   switch (diceNumber) {
     case 1:
       diceImage.src = "Images/Alea_1.png";
-      diceResult.appendChild("diceImage");
+      diceResult.appendChild(diceImage);
       break;
     case 2:
       diceImage.src = "Images/Alea_2.png";
-      diceResult.appendChild("diceImage");
+      diceResult.appendChild(diceImage);
       break;
     case 3:
       diceImage.src = "Images/Alea_3.png";
-      diceResult.appendChild("diceImage");
+      diceResult.appendChild(diceImage);
       break;
     case 4:
       diceImage.src = "Images/Alea_4.png";
-      diceResult.appendChild("diceImage");
+      diceResult.appendChild(diceImage);
       break;
     case 5:
       diceImage.src = "Images/Alea_5.png";
-      diceResult.appendChild("diceImage");
+      diceResult.appendChild(diceImage);
       break;
     case 6:
       diceImage.src = "Images/Alea_6.png";
-      diceResult.appendChild("diceImage");
+      diceResult.appendChild(diceImage);
       break;
   }
 };
@@ -57,7 +59,9 @@ let palyerRound = (diceNumber) => {
       let round = parseInt(roundPlayer1.innerHTML) + diceNumber;
       roundPlayer1.innerHTML = round.toString();
     }
-  }else if (getComputedStyle(player2Round).getPropertyValue("display") === "inline") {
+  } else if (
+    getComputedStyle(player2Round).getPropertyValue("display") === "inline"
+  ) {
     if (diceNumber === 1) {
       let round = 0;
       roundPlayer2.innerHTML = round.toString();
@@ -69,3 +73,57 @@ let palyerRound = (diceNumber) => {
     }
   }
 };
+
+hold.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  if (getComputedStyle(player1Round).getPropertyValue("display") === "inline") {
+    let roundPoint = parseInt(roundPlayer1.innerHTML);
+    let totalPoint = parseInt(globalPlayer1.innerHTML);
+    totalPoint += roundPoint;
+    globalPlayer1.innerHTML = totalPoint.toString();
+    roundPlayer1.innerHTML = "0";
+    player1Round.style.display = "none";
+    player2Round.style.display = "inline";
+  } else if (
+    getComputedStyle(player2Round).getPropertyValue("display") === "inline"
+  ) {
+    let roundPoint = parseInt(roundPlayer2.innerHTML);
+    let totalPoint = parseInt(globalPlayer2.innerHTML);
+    totalPoint += roundPoint;
+    globalPlayer2.innerHTML = totalPoint.toString();
+    roundPlayer2.innerHTML = "0";
+    player2Round.style.display = "none";
+    player1Round.style.display = "inline";
+  }
+
+  if (parseInt(globalPlayer1.innerHTML) >= 100) {
+    alert("Félicitation, le joueur 1 a gagné !!!!");
+    let replay = confirm("Voulez vous rejouer ?");
+
+    if (replay) {
+      newPart();
+    } else {
+      alert("Au revoir et à bientôt");
+    }
+  } else if (parseInt(globalPlayer2.innerHTML) >= 100) {
+    alert("Félicitation, le joueur 2 a gagné !!!!");
+    let replay = confirm("Voulez vous rejouer ?");
+
+    if (replay) {
+      newPart();
+    } else {
+      alert("Au revoir et à bientôt");
+    }
+  }
+});
+
+const newPart = () => {
+  let party = confirm("Une nouvelle partie remet les compteurs à zero !");
+
+  if (party) {
+    location.reload();
+  }
+};
+
+newGame.addEventListener("click", newPart);
